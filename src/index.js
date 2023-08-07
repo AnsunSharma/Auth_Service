509e61ac30a97421317971fc361fcf1b74b6e3b6
Ansun <Ansun.s@flyerssoft.com>
2023-08-07 09:06:00 +0530

    Auth service setup done

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express =require('express');
+const bodyParser = require('body-parser');
+const app =express()
+const { PORT } = require('./config/serverConfig');
+
+const prepareAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    // app.use('/api', apiRoutes);
+
+    app.listen(PORT, async () => {
+        console.log(`Server Started on Port: ${PORT}`);
+        // if(process.env.DB_SYNC) {
+        //     db.sequelize.sync({alter: true});
+        // }
+    });
+}   
+
+prepareAndStartServer();
